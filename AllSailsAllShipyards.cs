c7f3dae857afc86eae365fbfc046fbdcd0bb3de1
Nathaniel Edgar <nathaniel.edgar.fl@gmail.com>
2023-10-16 00:01:37 -0400

    main file skeleton

---
+++ AllSailsAllShipyards.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace AllSailsAllShipyards
+{
+    public class ModSettings : UnityModManager.ModSettings, IDrawable
+    {
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(modEntry);
+        }
+
+        public void OnChange() { }
+    }
+
+    static class Main
+    {
+        public static ModSettings settings;
+        public static UnityModManager.ModEntry.ModLogger logger;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            settings = UnityModManager.ModSettings.Load<ModSettings>(modEntry);
+            logger = modEntry.Logger;
+
+            modEntry.OnGUI = OnGUI;
+            modEntry.OnSaveGUI = OnSaveGUI;
+
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Draw(modEntry);
+        }
+
+        static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            settings.Save(modEntry);
+        }
+    }
+}
