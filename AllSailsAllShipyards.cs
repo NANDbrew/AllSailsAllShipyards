5c5136231d0e16b22a6fd2181381c1cfe2b5b661
Nathaniel Edgar <nathaniel.edgar.fl@gmail.com>
2023-10-16 18:42:07 -0400

    implementation complete

--- AllSailsAllShipyards.cs
+++ AllSailsAllShipyards.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using UnityModManagerNet;
@@ -10,7 +11,7 @@ namespace AllSailsAllShipyards
     {
         public override void Save(UnityModManager.ModEntry modEntry)
         {
-            Save(modEntry);
+            UnityModManager.ModSettings.Save(this, modEntry);
         }
 
         public void OnChange() { }
@@ -20,6 +21,7 @@ namespace AllSailsAllShipyards
     {
         public static ModSettings settings;
         public static UnityModManager.ModEntry.ModLogger logger;
+        public static GameObject[] completeShipyardList;
 
         static bool Load(UnityModManager.ModEntry modEntry)
         {
@@ -28,6 +30,7 @@ namespace AllSailsAllShipyards
 
             settings = UnityModManager.ModSettings.Load<ModSettings>(modEntry);
             logger = modEntry.Logger;
+            completeShipyardList = null;    // waiting to be filled by PrefabsDirectoryPatch
 
             modEntry.OnGUI = OnGUI;
             modEntry.OnSaveGUI = OnSaveGUI;
@@ -43,6 +46,58 @@ namespace AllSailsAllShipyards
         static void OnSaveGUI(UnityModManager.ModEntry modEntry)
         {
             settings.Save(modEntry);
+        }
+
+        public static string GetRegionNameFromId(int regionId)
+        {
+            switch (regionId)
+            {
+                case 0: return "Al'Ankh";
+                case 1: return "Emerald Archipelago";
+                case 2: return "Aestrin";
+                default: return "unknown";
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Shipyard))]
+    class ShipyardPatch
+    {
+        [HarmonyPatch("Awake")]
+        static void Postfix(ref int ___region, ref GameObject[] ___sailPrefabs)
+        {
+            Main.logger.Log(string.Format("Patching region {0}...", Main.GetRegionNameFromId(___region)));
+
+            if (Main.completeShipyardList != null)
+            {
+                ___sailPrefabs = Main.completeShipyardList;
+                Main.logger.Log(string.Format("Region {0} successfully patched.", Main.GetRegionNameFromId(___region)));
+            }
+            else
+            {
+                Main.logger.Log("Patch failed; complete sail prefabs list is not populated!");
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PrefabsDirectory))]
+    class PrefabsDirectoryPatch
+    {
+        [HarmonyPatch("Start")]
+        static void Postfix(ref GameObject[] ___sails)
+        {
+            Main.logger.Log("Beginning patch of PrefabsDirectory...");
+            var validSailPrefabs = new List<GameObject>();
+
+            for (int i = 0; i < ___sails.Length; i++)
+            {
+                // check for non-null value
+                if (___sails[i])
+                    validSailPrefabs.Add(___sails[i]);
+            }
+
+            Main.logger.Log(string.Format("Found {0} valid sail prefabs", validSailPrefabs.Count));
+            Main.completeShipyardList = validSailPrefabs.ToArray();
         }
     }
 }
