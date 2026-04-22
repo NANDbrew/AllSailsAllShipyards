526f2f2b283f7c591da5607880782438c6d1e4e3
NANDbrew <zortman@gmail.com>
2026-04-21 21:26:04 -0500

    Add patch priority (low) to sail list reader. This fixes compatibility issues with mods that add sails (FFL Paraw works. Not sure about others)

--- AllSailsAllShipyards.cs
+++ AllSailsAllShipyards.cs
@@ -69,6 +69,7 @@ namespace AllSailsAllShipyards
     class PrefabsDirectoryPatch
     {
         [HarmonyPatch("Start")]
+        [HarmonyPriority(Priority.VeryLow)]
         static void Postfix(ref GameObject[] ___sails)
         {
             var validSailPrefabs = new List<GameObject>();
